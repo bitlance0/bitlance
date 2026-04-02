2c25b4caf770e7cfbe671b7118d39fa7bb155a84
bitlance0 <bitlance0@gmail.com>
2026-04-02 01:15:27 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <CookieProvider>
           <AuthSync>
             <>
-              <WhatsAppFloatingButton phoneNumber="346124588073" />
+              <WhatsAppFloatingButton phoneNumber="34612458073" />
               {children}
               <CookieBanner />
             </>
